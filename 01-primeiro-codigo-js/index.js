3662060ee878c4c69ed9ff5dab0a478984c86ab3
Vinicius Vatte <vvatte73@gmail.com>
2025-09-03 18:45:18 -0300

    Declarando variaveis

--- 01-primeiro-codigo-js/index.js
+++ 01-primeiro-codigo-js/index.js
@@ -1,1 +1,5 @@
-window.alert("Olá mundo!")
+//window.alert("Olá mundo!")
+
+let retornoDoUsuario = confirm("Deseja continuar?")
+
+console.log(retornoDoUsuario)
---
+++ 03-variaveis/index.js
@@ -0,0 +1,11 @@
+// var nomePessoa = "Vinicius"
+
+// const emailPessoa = "vvatte73@gmail.com"
+
+// let telefonePessoa = "519999999"
+
+// nomePessoa = "Maria"
+
+let idade = prompt("Digite sua idade: ")
+
+console.log(idade)
