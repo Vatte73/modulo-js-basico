701942bcf688d9bab8d4177760978ef78538b31a
Vinicius Vatte <vvatte73@gmail.com>
2025-09-04 16:43:38 -0300

    Tipos primitivos de variaveis

--- 03-variaveis/index.js
+++ 03-variaveis/index.js
@@ -1,11 +1,9 @@
-// var nomePessoa = "Vinicius"
+var nomePessoa = "Vinicius"
 
-// const emailPessoa = "vvatte73@gmail.com"
+const emailPessoa = "vvatte73@gmail.com"
 
-// let telefonePessoa = "519999999"
+let telefonePessoa = "519999999"
 
-// nomePessoa = "Maria"
+nomePessoa = "Maria"
 
-let idade = prompt("Digite sua idade: ")
-
-console.log(idade)
+console.log(nomePessoa)
---
+++ 04-tipos-primitivos/index.js
@@ -0,0 +1,19 @@
+//Character
+let nome = "ABC 123 /"
+
+//Number
+let numero = 123
+let numeroComVirgula = 0.17
+let numeroNegativo = -59
+
+//Boolean
+let verdadeiro = true
+let falso = false
+
+// Null
+let pessoa = null
+
+
+
+
+
