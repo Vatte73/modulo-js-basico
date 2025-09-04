db908878b00ae399671eb67d8257507bc6a74549
Vinicius Vatte <vvatte73@gmail.com>
2025-09-04 19:20:24 -0300

    Tipos de laços For, Whie, do While

--- 07-lacos/index.js
+++ 07-lacos/index.js
@@ -1,3 +1,17 @@
 for(let i = 0; i < 5; i++){
     console.log(i)
 }
+
+let a = 0
+let b = 5
+
+while(a <= b){
+    console.log("incrementando a variave a " + a)
+    a++
+}
+
+var i = 1
+do{
+    console.log("Entrou " + i )
+    i++
+}while(i <= 5)
