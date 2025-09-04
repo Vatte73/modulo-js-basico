77b64664a27385dcc31ffd6fdba71647a64ab504
Vinicius Vatte <vvatte73@gmail.com>
2025-09-04 17:36:26 -0300

    Tipos de operadores lógicos

---
+++ 05-operadores-logicos/index.js
@@ -0,0 +1,27 @@
+/*
+    == ------ igual ------- a==b ------ verdadeiro se a for igual a b
+    === ------ idendentico ------- a===b ------ verdadeiro se a for idendentico a b
+
+    == ------ diferente ------- a!=b ------ verdadeiro se a for diferente a b
+    !== ------ não idendentico ------- a!==b ------ verdadeiro se a não for idendentico a b
+
+    < ------ menor ------- a < b ------ verdadeiro se a for menor que b
+    <= ------ menor ou igual ------- a <= b ------ verdadeiro se a for menor ou igual que b    
+
+    > ------ maior ------- a > b ------ verdadeiro se a for maior que b    
+    >= ------ maior ou igual ------- a >= b ------ verdadeiro se a for maior ou igual que b    
+
+    && ------ os dois operadosres devem retornar verdadeiro ------- a === b && a <= b ------ verdadeiro se as duas operações forem verdadeiras   
+
+    || ------ um dos dois operadosres deve retornar verdadeiro ------- a === b && a < b ------ verdadeiro se uma das operações forem verdadeiras  -------- OR
+*/
+
+const a = 3
+const b = 3
+
+console.log(a == b)
+
+
+
+
+
