edfda33400c16d8f77ca71a47abca4acbeef115a
Vinicius Vatte <vvatte73@gmail.com>
2025-09-04 18:24:42 -0300

    Tipos de operadores matemáticos

---
+++ 06-operadores-matematicos/index.js
@@ -0,0 +1,12 @@
+let PrimeiroValor = 8
+let segundoValor = 4
+
+console.log("Soma: ", PrimeiroValor + segundoValor)
+
+console.log("Subtração: ", PrimeiroValor - segundoValor)
+
+console.log("Multiplicação: ", PrimeiroValor * segundoValor)
+
+console.log("Divisão: ", PrimeiroValor / segundoValor)
+
+console.log("Resto: ", PrimeiroValor % segundoValor)
