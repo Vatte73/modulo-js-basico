b838921ee467433882cc29e74b0ab793913e89ee
Vinicius Vatte <vvatte73@gmail.com>
2025-10-01 20:20:43 -0300

    condicionais com swicth

---
+++ 09-condicionais-switch/index.js
@@ -0,0 +1,14 @@
+let nomeFilme = 'Rei Leão'
+
+
+switch (nomeFilme) {
+    case 'Batman':
+        console.log('Filme do Batman')
+        break
+    case 'Rei Leão':
+        console.log('Filme do Rei Leão')
+        break
+    default:
+        console.log('Filme não encontrado')
+        break
+}
