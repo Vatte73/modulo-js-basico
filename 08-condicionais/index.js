aab430b35042f695855a6b973efa2252a38a7e4a
Vinicius Vatte <vvatte73@gmail.com>
2025-10-01 19:32:02 -0300

    condicionais

---
+++ 08-condicionais/index.js
@@ -0,0 +1,15 @@
+let nomeFilme = 'Clube da luta'
+
+nomeFilme = 'Rei Leão'
+
+if (nomeFilme == 'Clube da luta') {
+    console.log('Você escolheu o filme', nomeFilme)
+}
+else if (nomeFilme == 'Rei Leão') {
+    console.log('Você escolheu o filme', nomeFilme)
+}
+else {
+    console.log('Filme não encontrado')
+}
+// COndição ? se ( true) : senao (false)
+nomeFilme === 'Rei Leão' ? console.log('Você escolheu o filme', nomeFilme) : console.log('Filme não encontrado')
