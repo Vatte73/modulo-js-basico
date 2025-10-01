2f13c8abe72ad076f00d16592aa147273f677964
Vinicius Vatte <vvatte73@gmail.com>
2025-10-01 20:50:39 -0300

    funções

---
+++ 10-funcoes/index.js
@@ -0,0 +1,17 @@
+
+function alertConquista(nome = 'usuário') {
+    alert("Parabéns " + nome + ", por ter chegado até aqui!")
+}
+
+alertConquista()
+
+
+function soma(a, b){
+   return a + b
+}
+
+let somaValores = soma(5, 3)
+console.log(somaValores)//8
+
+somaValores = soma(somaValores, 2)
+console.log(somaValores)//10
